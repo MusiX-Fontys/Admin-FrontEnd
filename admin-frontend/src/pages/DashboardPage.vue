1e22c6e5f8c25f00a954845832b1280f9919b03a
ruben <462404@student.fontys.nl>
2022-01-05 17:24:26 +0100

    Setup Admin Application

---
+++ admin-frontend/src/pages/DashboardPage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="dashboard">
+        <div id="loading">
+            <em class="icon fas fa-spinner"/>Loading...
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    components: {
+    },
+    created(){
+    },
+    methods: {
+    }
+}
+</script>
+<style scoped>
+    #dashboard{
+        display: flex;  
+        align-items: center;
+        justify-content: center;
+        height: calc(100% - 75px);
+        width: 100%;
+    }
+
+    #loading{
+        font-size: 4em;
+        color: var(--secondary-color);
+        display: flex;
+        align-items: center;
+    }
+
+    .icon {
+        margin-right: 5px;
+        animation: rotation 1.5s infinite linear;
+    }
+
+    @keyframes rotation {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(359deg);
+        }
+    }
+</style>
